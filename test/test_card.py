8071939a137358feda20235317dc379b9c4ae46f
J Seymour <JustJakeSeymour@gmail.com>
2023-07-05 12:51:28 -0600

    write more tests for card class

--- test/test_card.py
+++ test/test_card.py
@@ -4,11 +4,25 @@ sys.path.append('/Users/jseymour22/_projects/flashcards-python/lib/')
 from card import Card
 
 class TestCard(unittest.TestCase):
-    def test_card_properties(self):
+    def test_card_is_card(self):
+        # Card is of Card class
+        my_card = Card("Question", "Answer", "Category")
+        self.assertIsInstance(my_card, Card)
+
+    def test_card_questions(self):
+        # Card has a Quesiton
+        my_card = Card("Question", "Answer", "Category")
+        self.assertEqual(my_card.question, "Question")
+
+    def test_card_answers(self):
         # Card has an Answer
         my_card = Card("Question", "Answer", "Category")
-        my_card.answer = "Answer"
         self.assertEqual(my_card.answer, "Answer")
+
+    def test_card_categories(self):
+        # Card has a Category
+        my_card = Card("Question", "Answer", "Category")
+        self.assertEqual(my_card.category, "Category")
 
 if __name__ == '__main__':
         unittest.main()
