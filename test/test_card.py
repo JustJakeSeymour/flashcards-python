6ac7029f5ae10ed09f39fb06121888f88bfe0c79
J Seymour <JustJakeSeymour@gmail.com>
2023-07-05 13:07:17 -0600

    refactor test

--- test/test_card.py
+++ test/test_card.py
@@ -4,25 +4,24 @@ sys.path.append('/Users/jseymour22/_projects/flashcards-python/lib/')
 from card import Card
 
 class TestCard(unittest.TestCase):
+    def setUp(self):
+         self.my_card = Card('Question', 'Answer', 'Category')
+
     def test_card_is_card(self):
         # Card is of Card class
-        my_card = Card("Question", "Answer", "Category")
-        self.assertIsInstance(my_card, Card)
+        self.assertIsInstance(self.my_card, Card)
 
     def test_card_questions(self):
         # Card has a Quesiton
-        my_card = Card("Question", "Answer", "Category")
-        self.assertEqual(my_card.question, "Question")
+        self.assertEqual(self.my_card.question, 'Question')
 
     def test_card_answers(self):
         # Card has an Answer
-        my_card = Card("Question", "Answer", "Category")
-        self.assertEqual(my_card.answer, "Answer")
+        self.assertEqual(self.my_card.answer, 'Answer')
 
     def test_card_categories(self):
         # Card has a Category
-        my_card = Card("Question", "Answer", "Category")
-        self.assertEqual(my_card.category, "Category")
+        self.assertEqual(self.my_card.category, 'Category')
 
 if __name__ == '__main__':
         unittest.main()
