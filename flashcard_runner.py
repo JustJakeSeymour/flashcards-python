d251ce12c3c7d394275221b23bf7aded6647a48d
J Seymour <JustJakeSeymour@gmail.com>
2023-07-06 16:07:59 -0600

    scaffolding for flashcard runner

---
+++ flashcard_runner.py
@@ -0,0 +1,20 @@
+# load necessities
+
+# "Welcome! You're playing with %(len(deck.cards))s cards."
+
+# ---------------------------------
+
+# This is card number (n) out of (total_cards).
+
+# Question: (card.question)
+
+# user input
+
+# turn.feedback
+
+# ~loop~
+
+# ****** Game over! ******
+# You had (n) correct guesses out of (total_cards) for a total score of (percent_correct)
+# Category 1 - (percent correct by category)
+# Category 2 - (percent correct by category)
