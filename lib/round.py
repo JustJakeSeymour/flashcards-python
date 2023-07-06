54e233a4eae820d247b8c9de4508975195be6a96
J Seymour <JustJakeSeymour@gmail.com>
2023-07-06 15:41:15 -0600

    refactor and complete percent correct by category

--- lib/round.py
+++ lib/round.py
@@ -16,6 +16,15 @@ class Round:
         self.turns.append(current_turn)
         return current_turn
     
+    def category_turns(self, category):
+        category_turns = []
+        for turn in self.turns:
+            if turn.card.category == category:
+                category_turns.append(turn)
+            else:
+                pass
+        return category_turns    
+
     def correct_turns(self):
         correct = []
         for turn in self.turns:
@@ -30,9 +39,17 @@ class Round:
     
     def number_correct_by_category(self, category):
         category_correct = []
-        for turn in self.correct_turns():
-            if turn.card.category == category:
+        for turn in self.category_turns(category):
+            if turn.is_correct():
                 category_correct.append(turn)
             else:
                 pass
         return len(category_correct)
+    
+    def percent_correct(self):
+        percent = self.number_correct() / len(self.turns)*100
+        return round(percent, 1)
+
+    def percent_correct_by_category(self, category):
+        percent = self.number_correct_by_category(category) / len(self.category_turns(category))*100
+        return round(percent, 1)
--- test/test_round.py
+++ test/test_round.py
@@ -63,10 +63,22 @@ class TestRound(unittest.TestCase):
         self.assertEqual(self.round.number_correct_by_category('names'), 2)
     
     def test_round_percent_correct_method(self):
-        pass
+        self.round.take_turn('I do now!')
+        self.round.take_turn('Not Okay')
+        self.round.take_turn('Conrad')
+        self.round.take_turn('Jake')
+
+        self.assertEqual(self.round.percent_correct(), 75.0)
+        
     
     def test_round_percent_correct_by_category_method(self):
-        pass
+        self.round.take_turn('I do now!')
+        self.round.take_turn('Not Okay')
+        self.round.take_turn('Conrad')
+        self.round.take_turn('Jake')
+
+        self.assertEqual(self.round.percent_correct_by_category('meta'), 50.0)
+        self.assertEqual(self.round.percent_correct_by_category('names'), 100.0)
     
 if __name__ == '__main__':
         unittest.main()
